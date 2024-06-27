4d99ade89dcdd7ac201fe1992722a6769ad0e918
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-06-28 02:36:05 +0300

    try to fix problem images2

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,13 @@
 import globals from "globals";
 
-
 export default [
-  {languageOptions: { globals: globals.browser },
+  {
+    languageOptions: { globals: globals.browser },
     rules: {
       indent: ['error', 4],
-      quotes: ['error','single'],
+      quotes: ['error', 'single'],
       semi: ['error', 'always'],
-    }
-
-
-
-},
+    },
+    ignorePatterns: ['public/**'], // הוספת השורה הזו כדי להתעלם מתיקיית public
+  },
 ];
