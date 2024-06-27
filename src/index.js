301dcb2836e833b366c54127ca31dd4666b11a36
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-06-28 01:54:58 +0300

    FIX lint (0 ERROR)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port= 3000;
 
-app.get("/", (req, res) => {
- const response='{ "text": "Hello from sss^erver!"}';
- res.send(response);
+app.get('/', (req, res) => {
+    const response='{ "text": "Hello from sss^erver!"}';
+    res.send(response);
 });
 
 app.listen(port, ()=>{
