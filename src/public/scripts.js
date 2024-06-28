dc8ac31f566fcf283418f8ef792d7c0e3d3bf1ca
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-06-28 08:33:34 +0300

    try js work (3)

--- src/public/scripts.js
+++ src/public/scripts.js
@@ -1,4 +1,7 @@
+console.log('script.js loaded');
+
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
     document.querySelectorAll('.navbar a').forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
@@ -7,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     function loadPage(url) {
+        console.log(`Loading page: ${url}`);
         fetch(url)
             .then(response => response.text())
             .then(html => {
