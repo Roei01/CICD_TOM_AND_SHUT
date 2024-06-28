4b7c024af229181ed075ac9147707ed3618af5c5
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-06-28 03:05:15 +0300

    test

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,12 @@ import globals from "globals";
 
 export default [
   {
+    ignores: ["src/public/**"], // שימוש במאפיין ignores במקום ignorePatterns
     languageOptions: { globals: globals.browser },
     rules: {
       indent: ['error', 4],
       quotes: ['error', 'single'],
       semi: ['error', 'always'],
-    },
-    ignorePatterns: ['src/public/**'], // התעלמות מהתיקייה src/public
+    }
   },
 ];
