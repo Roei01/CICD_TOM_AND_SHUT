9e73283e4a31eff54516d2ab07d1ab70add7d3e2
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-06-28 08:09:12 +0300

    update java script (try)

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 export default [
   {
     ignores: ["src/public/**"], // שימוש במאפיין ignores במקום ignorePatterns
+    ignores: ["src/**"], // שימוש במאפיין ignores במקום ignorePatterns
     languageOptions: { globals: globals.browser },
     rules: {
       indent: ['error', 4],
