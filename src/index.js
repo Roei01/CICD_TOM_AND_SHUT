c5c54ffe64061f74a148a6e645820c37e180db91
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-07-01 22:47:16 +0300

    update

--- src/index.js
+++ src/index.js
@@ -104,7 +104,7 @@ app.get('/', (req, res) => {
     const content = `
     <h2>Welcome to Sushi Store</h2>
     <p class="home-text">Enjoy the best sushi in town. Explore our menu and learn more about us.</p>
-    <img class="home-image" src="/images/sushi.jpg" alt="Sushi">
+    <img class="home-image" src="/images/pexels-cottonbro-3297801.jpg" alt="Sushi">
     <a href="/menu" onclick="event.preventDefault(); loadPage('/menu');" class="btn">Explore Menu</a>
     `;
     res.send(generatePage('Sushi Store - Home', content));
