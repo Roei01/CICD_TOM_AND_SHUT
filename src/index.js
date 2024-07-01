3dd8bfd9be995380a55e0b080f7abbf9292cf09d
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-07-01 08:49:24 +0300

    Check on shopping on the website(4)

--- src/index.js
+++ src/index.js
@@ -384,7 +384,7 @@ app.get('/', (req, res) => {
     const content = `
     <h2>Welcome to Sushi Store</h2>
     <p class="home-text">Enjoy the best sushi in town. Explore our menu and learn more about us.</p>
-    <img class="home-image" src="/images/sushi.jpg" alt="Sushi">
+    <img class="home-image" src="/images/pexels-isabella-mendes-107313-858501.jpg" alt="Sushi">
     <a href="/menu" onclick="event.preventDefault(); loadPage('/menu');" class="btn">Explore Menu</a>
     `;
     res.send(generatePage('Sushi Store - Home', content));
